010e9c6793cf29f717cc6b54138abe5ab5486c78
Joonas Korkealaakso <joonas.korkealaakso@affecto.com>
2015-07-22 11:19:16 +0300

    My completed cachematrix functions for Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,10 @@
-## The following functions are used to store a matrix and
-## then to solve the inverse matrix of the stored matrix.
+## The following functions stores a matrix in cache memory
+## and then solves the inverse matrix of this cached matrix.
 
-## the 'makeCacheMatrix' function below creates a cache  
-## matrix in the form of a list that allows set and get
-## the value for the cached matrix and its inverse matrix.
+## the 'makeCacheMatrix' function below creates a special
+## matrix in the form of a list which contains functions to
+## set and get the values of the cache matrix and the values
+## of its inverse matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -20,11 +21,12 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 ## The 'cacheSolve' function calculates the inverse matrix
-## of the cached matrix from the function 'makeCacheMatrix'
-## by first retrieving the inverse matrix if it has already
-## been calculated or optionally solves the inverse matrix
-## and caches the results if the inverse matrix was not
-## previously cached.
+## of the cache matrix created with the 'makeCacheMatrix'
+## function. The 'cacheSolve' function first attempts to 
+## retrieve the inverse matrix from the cache. If the inverse
+## matrix has not been calculated the 'cacheSolve' function
+## solves the inverse matrix and sets the inverse matrix in
+## the cache with the setInv function.
 
 cacheSolve <- function(x, ...) {
         m <- x$getInv()
